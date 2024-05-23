56a904d09cbcdda89a8b880c0f44c7be1a3dac9a
dikistutmazsabri <polat133@yahoo.com>
2024-05-23 20:52:21 +0300

    Initial Commit

---
+++ kapital1/kapital1App.swift
@@ -0,0 +1,17 @@
+//
+//  kapital1App.swift
+//  kapital1
+//
+//  Created by Burak Polat on 23.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct kapital1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
